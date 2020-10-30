0d44878f1f92af308bdf22ea847dbaaaa90653ec
Avanee Dheer <avanee.dheer_cs19@gla.ac.in>
2020-10-30 16:52:12 +0530

    overriding run method for Tortoise and adding cases in switch blocks

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -9,10 +9,34 @@ public class Tortoise extends Thread {
         this.tortoise = new Thread(this, "tortoise");
     }
 
+    public Thread getTortoise() {
+        return tortoise;
+    }
+
+    @Override
+    public void run() {
+        switch (0) {
+            case 0:
+                System.out.println("The Tortoise has started from the start line!");
+                break;
+
+        }
+        switch (1) {
+            case 0:
+                System.out.println("The Tortoise has started from the start line!");
+                break;
+            case 1:
+                System.out.println("The Tortoise has reached the Cool River!");
+                break;
+            case 2:
+                System.out.println("The Tortoise has reached the Mountain Hill!");
+                break;
+            case 3:
+                System.out.println("The Tortoise has reached the Big Oak Tree!");
+                break;
+            case 4:
+                System.out.println("The Tortoise has reached the finish line!");
+                break;
+        }
+    }
 }
-
-
-
-
-
-
